1209cd8e69712acbd71e097f915a44da4e8a23e4
Maciej Barczykowski <barczykowskimaciej@gmail.com>
2023-03-26 20:00:25 +0200

    Pierwsze Testowanie

---
+++ TestProject1/EmployeeScoreTest.cs
@@ -0,0 +1,22 @@
+using Apka_Szkoleniowa;
+
+namespace Pierwsze_Testowanie
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var employee = new Employee("Maciek", "Nowak", 45);
+            employee.AddScore(5);
+            employee.AddScore(2);
+            employee.AddScore(8);
+            employee.AddScore(-15);
+            
+           var result = employee.Result;
+
+            Assert.AreEqual(0, result);
+        }
+
+    }
+}
