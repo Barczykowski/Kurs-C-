152f186cd72bf42c044c1e4e06e559bd7184dcbd
Maciej Barczykowski <barczykowskimaciej@gmail.com>
2023-04-23 14:15:37 +0200

    Dzien 15

--- Apka Szkoleniowa/Supervisor.cs
+++ Apka Szkoleniowa/Supervisor.cs
@@ -2,16 +2,91 @@
 {
     public class Supervisor : IEmployee
     {
-        public string Name => throw new NotImplementedException();
+        private List<float> grades = new List<float>();
 
-        public string Surname => throw new NotImplementedException();
-
-        public void AddGrade(int grade)
+        public Supervisor(string name, string surname)
         {
-            throw new NotImplementedException();
+            this.Name = name;
+            this.Surname = surname;
         }
 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+   
+
         public void AddGrade(string grade)
+        
+            {
+                switch (grade)
+                {
+                    case "1":
+                        this.grades.Add(0);
+                        break;
+
+                    case "2-":
+                        this.grades.Add(15);
+                        break;
+
+                    case "2":
+                        this.grades.Add(20);
+                        break;
+
+                    case "2+":
+                        this.grades.Add(25);
+                        break;
+
+                    case "3-":
+                        this.grades.Add(35);
+                        break;
+
+                    case "3":
+                        this.grades.Add(40);
+                        break;
+
+                    case "3+":
+                         this.grades.Add(45);
+                         break;
+
+                    case "4-":
+                        this.grades.Add(55);
+                        break;
+
+                    case "4":
+                        this.grades.Add(60);
+                        break;
+
+                    case "4+":
+                        this.grades.Add(65);
+                        break;
+
+                    case "5-":
+                        this.grades.Add(75);
+                        break;
+                  
+                    case "5":
+                        this.grades.Add(80);
+                        break;
+
+                    case "5+":
+                        this.grades.Add(85);
+                        break;
+
+                    case "6-":
+                        this.grades.Add(95);
+                        break;
+
+                    case "6":
+                        this.grades.Add(100);
+                        break;
+                    
+                    default:
+
+                        throw new Exception("podaj ocenę od 1 do 6");
+                }
+            }
+
+        public void AddGrade(int grade)
         {
             throw new NotImplementedException();
         }
@@ -37,3 +112,6 @@
         }
     }
 }
+
+
+
