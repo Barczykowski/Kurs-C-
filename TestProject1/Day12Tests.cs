27a0c80a440fb8441b330948f5ad315d04120ad6
Maciej Barczykowski <barczykowskimaciej@gmail.com>
2023-04-23 14:15:43 +0200

    Dzien 15

---
+++ TestProject1/Day12Tests.cs
@@ -0,0 +1,23 @@
+﻿using Apka_Szkoleniowa;
+using System.ComponentModel;
+
+public class Day12Tests
+
+{
+    [Test]
+    public void TestingGradesAsString()
+    {
+    
+    var Employee = new Employee();
+
+        var statistics = Employee.GetStatistics().Min;
+
+        Employee.AddGrade(20);
+        Employee.AddGrade(30);
+        Employee.AddGrade(40);
+
+
+       Assert.AreEqual(statistics, 20);
+
+    }
+}
---
+++ TestProject1/Statistics.cs
@@ -0,0 +1,15 @@
+﻿namespace Apka_Szkoleniowa
+{
+    public class Statistics
+    {
+        public float Min { get; set; }
+
+        public float Max { get; set; }
+
+        public float Average { get; set; }
+
+        public char AverageLetter { get; set; }
+
+
+    }
+}
